65d5b1aa45d63637edc851351ab54c3b0ca7b083
Sue Xiong <sxiong0519@gmail.com>
2021-08-24 14:47:22 -0500

    csharp fizzbuzz

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿// function main() {
+//   for(let n=1; n<=100; n++) {
+//     if (n % 15 === 0) {
+//       console.log("fizzbuzz");
+//     } else if (n % 3 === 0) {
+//       console.log("fizz");
+//     } else if (n % 5 === 0) {
+//       console.log("buzz");
+//     } else {
+//       console.log(n);
+//     }
+//   }
+// }
+
+// main();
+
+using System;
+
+for (int n = 1; n <= 100; n++)
+{
+    if (n % 15 == 0) 
+    {
+        //cannot print out both int and string in the same function... must separate them
+      Console.Write(n);  
+      Console.WriteLine(" fizzbuzz");
+    } 
+    else if (n % 3 == 0) 
+    {
+      Console.Write(n);
+      Console.WriteLine(" fizz");
+    } 
+    else if (n % 5 == 0) 
+    {
+      Console.Write(n);  
+      Console.WriteLine(" buzz");
+    } 
+    else 
+    {
+      Console.WriteLine(n);
+    }
+}
